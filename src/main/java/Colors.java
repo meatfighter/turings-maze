2eb94f04a29f7194c76b2246ce05341dc5dc4927
Michael Birken <o__1@hotmail.com>
2023-06-17 15:07:51 -0400

    creating

---
+++ src/main/java/Colors.java
@@ -0,0 +1,6 @@
+public interface Colors {
+    int BLACK = 0x000000;
+    int RED = 0xFF0000;
+    int GREEN = 0x00FF00;
+    int GRAY = 0x808080;
+}
---
+++ src/main/java/Tiles.java
@@ -0,0 +1,6 @@
+public interface Tiles {
+    byte BLACK = 0;
+    byte GRAY = 1;
+    byte RED = 2;
+    byte GREEN = 3;
+}
